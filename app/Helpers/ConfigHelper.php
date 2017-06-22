cc8bd1d5886be77e2ab3b075f0b4acd5ba782f46
Ashley Sheridan <ash@ashleysheridan.co.uk>
2017-06-22 03:49:00 +0100

    fixed exception bug in config helper

--- app/Helpers/ConfigHelper.php
+++ app/Helpers/ConfigHelper.php
@@ -33,16 +33,21 @@ class ConfigHelper
 	public function get_for_overlay($overlay, $option)
 	{
 		$overlay_config_value = config("campaigns.$this->campaign_name.overlays.$overlay.$option");
-		$fallback_config_value = $this->get($option);
 		
 		if(!is_null($overlay_config_value) )
 			return $overlay_config_value;
 		else
 		{
-			if(!is_null($fallback_config_value) )
+			try
+			{
+				$fallback_config_value = $this->get($option);
+				
 				return $fallback_config_value;
-			else
+			}
+			catch (MissingConfigOptionException $e)
+			{
 				throw new \App\Exceptions\MissingConfigOptionException("$option is missing in overlay config and no fallback available");
+			}
 		}
 	}
 }
