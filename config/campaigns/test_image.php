e42778b699fc1175e7d1f33bcf4e5793657a1df5
Ashley Sheridan <ash@ashleysheridan.co.uk>
2017-06-26 21:30:38 +0100

    added image layer campaign test file

---
+++ config/campaigns/test_image.php
@@ -0,0 +1,21 @@
+<?php
+return array(
+	'type'			=> 'flat',
+	'description'	=> '',
+	'format'		=> 'png',
+	'width'			=> 430,
+	'height'		=> 500,
+	'background'	=> '#fff',
+	'overlays'    => array(
+		'emoji' => array(
+			'type'				=> 'image',
+			'url'				=> array(
+				'lynx/pack-body-spray.jpg',
+				'lynx/pack-cream-gel.jpg',
+				'lynx/pack-roll-on.jpg',
+			),
+			'x'					=> 0,
+			'y'					=> 100,
+		),
+	),
+);
