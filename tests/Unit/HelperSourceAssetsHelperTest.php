f37b5eaa49fa976d406a525f4dad9e618ae82288
Ashley Sheridan <ash@ashleysheridan.co.uk>
2018-01-25 00:03:02 +0000

    added final tests for source assets helper class

--- tests/Unit/HelperSourceAssetsHelperTest.php
+++ tests/Unit/HelperSourceAssetsHelperTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use App\Helpers\FileMimeHelper as FileMimeHelper;
 use App\Helpers\SourceAssetsHelper as SourceAssetsHelper;
 use App\Entities\ImageMime as ImageMime;
+use App\Entities\iImageProperties as iImageProperties;
 
 class HelperSourceAssetsHelperTest extends TestCase
 {
@@ -77,6 +78,87 @@ class HelperSourceAssetsHelperTest extends TestCase
 		$this->assertEquals($base_image, 'gif image');
 	}
 
+	public function testGetBaseImageFromCreateBaseImageFromExisting()
+	{
+		$base_uri = 'some filename';
+
+		$mock_image_properties = $this->getMockBuilder('App\Entities\iImageProperties')
+			->getMock();
+
+		$mock_assets_helper = $this->getMockBuilder(SourceAssetsHelper::class)
+			->setMethods(['__construct', 'get_real_source_path', 'load_base_image', 'get_mime_type_from_filename'])
+			->disableOriginalConstructor()
+			->getMock();
+		
+		$mock_image_mime = $this->getMockBuilder('App\Entities\ImageMime')
+			->getMock();
+		$mock_gd_image = imagecreatetruecolor(1, 1);
+
+		$mock_assets_helper->expects($this->once())
+			->method('get_real_source_path');
+		$mock_assets_helper->expects($this->once())
+			->method('get_mime_type_from_filename')
+			->willReturn($mock_image_mime);
+		$mock_assets_helper->expects($this->once())
+			->method('load_base_image')
+			->willReturn($mock_gd_image);
+		
+		$image_data = $mock_assets_helper->create_base_image_from_existing($base_uri, $mock_image_properties);
+		
+		$this->assertEquals(get_resource_type($image_data), 'gd');
+	}
+	
+	public function testImagePropertiesSetFromCreateBaseImageFromExisting()
+	{
+		$base_uri = 'some filename';
+
+		$mock_image_properties = $this->getMockBuilder('App\Entities\iImageProperties')
+			->setMethods(['set_uri', 'set_dimensions', 'set_mime', 'get_width', 'get_height', 'get_mime', 'get_mime_string'])
+			->getMock();
+
+		$mock_assets_helper = $this->getMockBuilder(SourceAssetsHelper::class)
+			->setMethods(['__construct', 'get_real_source_path', 'load_base_image', 'get_mime_type_from_filename'])
+			->disableOriginalConstructor()
+			->getMock();
+		
+		$mock_image_mime = $this->getMockBuilder('App\Entities\ImageMime')
+			->getMock();
+		$mock_gd_image = imagecreatetruecolor(1, 1);
+		
+		$mock_assets_helper->method('get_mime_type_from_filename')
+			->willReturn($mock_image_mime);
+		$mock_assets_helper->method('load_base_image')
+			->willReturn($mock_gd_image);
+		
+		$mock_image_properties->expects($this->once())
+			->method('set_uri');
+		$mock_image_properties->expects($this->once())
+			->method('set_dimensions');
+		$mock_image_properties->expects($this->once())
+			->method('set_mime');
+
+		$mock_assets_helper->create_base_image_from_existing($base_uri, $mock_image_properties);
+	}
+	
+	public function testGetMimeTypeFromFilename()
+	{
+		$filename = 'some filename';
+		$some_mime = 'some mime type';
+		$path_helper =  new PathHelper();
+		$mock_file_mime_helper = $this->getMockBuilder('App\Helpers\FileMimeHelper')
+			->setMethods(['get_mime_type_from_filename'])
+			->getMock();
+		
+		$mock_file_mime_helper->expects($this->once())
+			->method('get_mime_type_from_filename')
+			->willReturn($some_mime);
+		
+		$assets_helper = new SourceAssetsHelper($mock_file_mime_helper, $path_helper);
+		
+		$mime_type = $assets_helper->get_mime_type_from_filename($filename);
+		
+		$this->assertEquals($mime_type, $some_mime);
+	}
 }
 
 class mockPathHelper extends PathHelper
