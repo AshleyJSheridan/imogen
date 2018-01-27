58758167dbf81010bd08b530fb34253898b38fe0
Ashley Sheridan <ash@ashleysheridan.co.uk>
2018-01-27 12:11:21 +0000

    added test for config request substitution helper

---
+++ tests/Unit/HelperConfigRequestSubstitutionHelperTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Helpers;
+
+use Tests\TestCase;
+use Illuminate\Http\Request;
+
+class HelperConfigRequestSubstitutionHelper extends TestCase
+{
+	private $helper;
+	
+	protected function setUp()
+	{
+		$request = new MockRequest();
+		
+		$this->helper = new ConfigRequestSubstitutionHelper($request);
+	}
+	
+	public function testSourceAssetsHelperConstruction()
+	{
+		$this->assertTrue($this->helper instanceof ConfigRequestSubstitutionHelper);
+	}
+	
+	public function testSubstituteText()
+	{
+		$original_text = "some text {{with}} {{placeholders}}";
+		$expected_text = "some text ~~with~~ ~~placeholders~~";
+		
+		$substituted_text = $this->helper->substitute_content_with_request_params($original_text);
+		
+		$this->assertEquals($substituted_text, $expected_text);
+	}
+	
+	public function testSubstituteTextWithMissingParams()
+	{
+		$request = new MockRequestMissing();
+		
+		$helper = new ConfigRequestSubstitutionHelper($request);
+		
+		$original_text = "some text {{with}} {{placeholders}}";
+		$expected_text = "some text  ";
+		
+		$substituted_text = $helper->substitute_content_with_request_params($original_text);
+		
+		$this->assertEquals($substituted_text, $expected_text);
+	}
+}
+
+class MockRequest extends Request
+{
+	public function get($param_key, $default = null)
+	{
+		return "~~$param_key~~";
+	}
+}
+
+class MockRequestMissing extends Request
+{
+	public function get($param_key, $default = null)
+	{
+		return "";
+	}
+}
